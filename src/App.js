4ea8b2e9cc2f3bd7fe0b1b6076f7475dad4982f4
Sanjay Sharma <aniruddh.a@neosoftmail.com>
2023-04-12 18:03:54 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ import "./style.css"
 import Login from './pages/Login';
 import { BrowserRouter , Routes, Route} from "react-router-dom"
 import Register from '../src/pages/Register';
-import { useSelector } from "react-redux"
 import Dashboard from '../src/pages/Dashboard';
+import ProtectedRoute from './pages/Protected';
 
 function App() {
 
   
   
-  const userToken = useSelector((store) => store.credential.user?.accessToken)
+  
   return (
     <BrowserRouter>
     <div className="App">
       <Routes>
-        {userToken && <Route path='/' element={<Dashboard/>}/>}
         <Route path='/login' element={<Login/>}/>
+        <Route path="/" element={
+        <ProtectedRoute>
+        <Dashboard/>
+        </ProtectedRoute>
+     }/>
         <Route path='/register' element={<Register/>}/>
     </Routes>
     </div>
