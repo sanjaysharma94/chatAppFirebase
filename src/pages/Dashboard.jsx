fb94d4542d6d83d8473db04b30428b8669bb42f2
Sanjay Sharma <aniruddh.a@neosoftmail.com>
2023-04-12 18:15:20 +0530

    Protected  Routes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,10 +2,17 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { sendEmailVerification } from "firebase/auth";
 import {auth} from "../firebase"
+import {Navigate, useLocation} from "react-router-dom"
 
 function Dashboard() {
+  let location = useLocation();
   const user = useSelector((store) => store.credential.user);
-  console.log(user)
+  let token = sessionStorage.getItem("usertoken")
+  // const userDetail = 
+  token = JSON.parse(token)
+  document.cookie = ` bearer = ${token.stsTokenManager.accessToken} ;expires=wed , 12 Apr 2023 13:15:00 UTC; path=/;`
+  console.log("coockies",(document.cookie))
+
 
   const VerifyEmail = async () => {
     try {
@@ -20,6 +27,8 @@ function Dashboard() {
     <div>
       welcome to main chat page
       <button onClick={VerifyEmail}>Verify Email</button>
+      <h1>{token.displayName}</h1>
+      
     </div>
   );
 }
